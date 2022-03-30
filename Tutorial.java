c44535717fff22252cb67635c1b458dd8938aaa2
Alexander Benak <benaka@bzz.ch>
2022-03-30 14:23:14 +0200

    Code refactoring + new method

--- Tutorial.java
+++ Tutorial.java
@@ -4,10 +4,9 @@ import java.awt.*;
 public class Tutorial extends JFrame {
 
     JPanel btnPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-
     JPanel searchpanel = new JPanel();
+
     JTextField searchbar = new JTextField("");
-    JButton search = new JButton("suchen");
 
     JFrame frame1 = new JFrame();
     JFrame frame2 = new JFrame();
@@ -16,26 +15,33 @@ public class Tutorial extends JFrame {
     JButton next = new JButton(">");
     JButton back = new JButton("<");
     JButton close = new JButton("X");
+    JButton search = new JButton("suchen");
 
     public Tutorial(){
-        frame1.setSize(500,500);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        addButtons(frame1);
+        addButtons(frame2);
+        addButtons(frame3);
+    }
+
+    public static void main(String[] args) {
+        new Tutorial();
+    }
+
+    public void addButtons(JFrame frame){
+        frame.setSize(500,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         btnPanel.add(close);
         btnPanel.add(back);
         btnPanel.add(next);
 
-        frame1.getContentPane().add(btnPanel, BorderLayout.SOUTH);
+        frame.getContentPane().add(btnPanel, BorderLayout.SOUTH);
         searchpanel.setLayout(new FlowLayout());
         frame1.getContentPane().add(searchpanel, BorderLayout.NORTH);
         searchbar.setPreferredSize(new Dimension(220, 30));
         searchpanel.add(searchbar);
         searchpanel.add(search);
         frame1.setVisible(true);
-    }
-
-    public static void main(String[] args) {
-        new Tutorial();
     }
 
 }
