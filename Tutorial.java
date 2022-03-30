0d9be2b402d6a8908f3d15fe9fd3bfff59b5c2ed
diego <fantinod@bzz.ch>
2022-03-30 14:20:55 +0200

    searchbar

--- Tutorial.java
+++ Tutorial.java
@@ -5,6 +5,10 @@ public class Tutorial extends JFrame {
 
     JPanel btnPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
 
+    JPanel searchpanel = new JPanel();
+    JTextField searchbar = new JTextField("");
+    JButton search = new JButton("suchen");
+
     JFrame frame1 = new JFrame();
     JFrame frame2 = new JFrame();
     JFrame frame3 = new JFrame();
@@ -22,7 +26,11 @@ public class Tutorial extends JFrame {
         btnPanel.add(next);
 
         frame1.getContentPane().add(btnPanel, BorderLayout.SOUTH);
-
+        searchpanel.setLayout(new FlowLayout());
+        frame1.getContentPane().add(searchpanel, BorderLayout.NORTH);
+        searchbar.setPreferredSize(new Dimension(220, 30));
+        searchpanel.add(searchbar);
+        searchpanel.add(search);
         frame1.setVisible(true);
     }
 
